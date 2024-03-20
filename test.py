51d4998c10177efae5c3bb2b7d8762d1d3df0e15
berrazuriz1 <berrazuriz1@uc.cl>
2024-03-20 20:43:12 -0300

    second commit

---
+++ test.py
@@ -0,0 +1,18 @@
+import test2
+
+def hello():
+  a = 1
+  print('Hello, world!')
+
+def factorial(n):
+  hello()
+  result = 1
+  for i in range(1, n + 1):
+    result *= i
+  return result
+
+def fibonacci(n):
+  a, b = 0, 1
+  while a < n:
+    print(a, end=' ')
+    a, b = b, a + b
