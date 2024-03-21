f7f7d24a7333f94bca0088da7c77ae19a160cc2d
Pepe <jndominguez@uc.cl>
2024-03-21 16:26:05 -0300

    added all files

--- src/graph_construction/graph_builder.py
+++ src/graph_construction/graph_builder.py
@@ -6,74 +6,162 @@ from llama_index.core.schema import NodeRelationship
 from neo4j_manager import Neo4jManager
 from pathlib import Path
 import os
+import uuid
+
 
 class GraphConstructor:
-  NODES_NAME_MAP = {
-    'file': 'FILE',
-    'function_definition': 'FUNCTION',
-    'class_definition': 'CLASS',
-  }
-
-  RELATIONS_TYPES_MAP = {
-    'function_definition': 'FUNCTION_DEFINITION',
-    'class_definition': 'CLASS_DEFINITION',
-  }
-
-  def __init__(self, graph_manager: Neo4jManager):
-    self.graph_manager = graph_manager
-  
-  def __process_node__(self, node):
-    is_parent_node = False
-    relationships = []
-    for relation in node.relationships.items():
-      if relation[0] == NodeRelationship.CHILD:
-        for child in relation[1]:
-          relation_type = child.metadata['inclusive_scopes'][0]['type'] if child.metadata['inclusive_scopes'] else ''
-          relationships.append({'sourceId': node.node_id, 'targetId': child.node_id, 'type': self.RELATIONS_TYPES_MAP.get(relation_type, 'UNKNOWN')})
-      elif relation[0] == NodeRelationship.PARENT:
-        if relation[1] is None:
-          is_parent_node = True
-
-    typeNode = 'file' if is_parent_node else node.metadata['inclusive_scopes'][0]['type'] if node.metadata['inclusive_scopes'] else ''
-    processed_node = {
-      'type': self.NODES_NAME_MAP.get(typeNode, 'UNKNOWN'),
-      'attributes': {
-        'language': node.metadata['language'],
-        'text': node.text,
-        'node_id': node.node_id,
-      },
+    NODES_NAME_MAP = {
+        "file": "FILE",
+        "function_definition": "FUNCTION",
+        "class_definition": "CLASS",
     }
-    return processed_node, relationships
 
-  def process_file(self, file_path, languaje):
-    path = Path(file_path)
-    if not path.exists():
-      print(f'File {file_path} does not exist.')
-      return
-    documents = SimpleDirectoryReader(
-      input_files=[path],
-      file_metadata=lambda x: {'filepath': x},
-    ).load_data()
+    RELATIONS_TYPES_MAP = {
+        "function_definition": "FUNCTION_DEFINITION",
+        "class_definition": "CLASS_DEFINITION",
+    }
 
-    code = CodeHierarchyNodeParser(
-      language=languaje,
-      chunk_min_characters=3,
-      code_splitter=CodeSplitter(language=languaje, max_chars=1000, chunk_lines=10),
-    )
+    def __init__(self, graph_manager: Neo4jManager):
+        self.graph_manager = graph_manager
 
-    split_nodes = code.get_nodes_from_documents(documents)
-    node_list = []
-    edges_list = []
-    for node in split_nodes:
-      processed_node, relationships = self.__process_node__(node)
-      node_list.append(processed_node)
-      edges_list.extend(relationships)
+    def __process_node__(self, node):
+        is_parent_node = False
+        relationships = []
+        for relation in node.relationships.items():
+            if relation[0] == NodeRelationship.CHILD:
+                for child in relation[1]:
+                    relation_type = (
+                        child.metadata["inclusive_scopes"][0]["type"]
+                        if child.metadata["inclusive_scopes"]
+                        else ""
+                    )
+                    relationships.append(
+                        {
+                            "sourceId": node.node_id,
+                            "targetId": child.node_id,
+                            "type": self.RELATIONS_TYPES_MAP.get(
+                                relation_type, "UNKNOWN"
+                            ),
+                        }
+                    )
+            elif relation[0] == NodeRelationship.PARENT:
+                if relation[1] is None:
+                    is_parent_node = True
 
-    self.graph_manager.create_nodes(node_list)
-    self.graph_manager.create_edges(edges_list)
+        typeNode = (
+            "file"
+            if is_parent_node
+            else (
+                node.metadata["inclusive_scopes"][0]["type"]
+                if node.metadata["inclusive_scopes"]
+                else ""
+            )
+        )
+        processed_node = {
+            "type": self.NODES_NAME_MAP.get(typeNode, "UNKNOWN"),
+            "attributes": {
+                "language": node.metadata["language"],
+                "text": node.text,
+                "node_id": node.node_id,
+            },
+        }
+        return processed_node, relationships
 
-if __name__ == '__main__':
-  graph_manager = Neo4jManager()
-  graph_constructor = GraphConstructor(graph_manager)
-  graph_constructor.process_file('src/test_documents/test.py', 'python')
-  graph_manager.close()
+    def scan_directory(self, path, nodes=[], relationships=[], parent_id=None):
+        package = False
+        init_py_path = os.path.join(path, "__init__.py")
+        if os.path.exists(init_py_path):
+            package = True
+        node_type = "Package" if package else "Folder"
+        directory_node = {
+            "type": node_type,
+            "attributes": {
+                "path": path,
+                "name": os.path.basename(path),
+                "node_id": str(uuid.uuid4()),
+            },
+        }
+        if parent_id is not None:
+            relationships.append(
+                {
+                    "sourceId": parent_id,
+                    "targetId": directory_node["node_id"],
+                    "type": "contains",
+                }
+            )
+        nodes.append(directory_node)
+        for entry in os.scandir(path):
+            if entry.is_file():
+                if entry.name.endswith(".py") and not entry.name == ("__init__.py"):
+                    # imports, function_imports, relative_imports = get_imports(entry.path)
+                    node_list, edge_list = self.process_file(entry.path)
+                    nodes.extend(node_list)
+                    relationships.extend(edge_list)
+                    relationships.append(
+                        {
+                            "sourceId": directory_node["node_id"],
+                            "targetId": node_list[0]["node_id"],
+                            "type": "contains",
+                        }
+                    )
+                else:
+                    file_node = {
+                        "type": "File",
+                        "attributes": {
+                            "path": entry.path,
+                            "name": entry.name,
+                            "node_id": str(uuid.uuid4()),
+                        },
+                    }
+                    nodes.append(file_node)
+                    relationships.append(
+                        {
+                            "sourceId": directory_node["node_id"],
+                            "targetId": file_node["node_id"],
+                            "type": "contains",
+                        }
+                    )
+            if entry.is_dir():
+                nodes, relationships = self.scan_directory(
+                    entry.path, nodes, relationships, directory_node["node_id"]
+                )
+        return nodes, relationships
+
+    def build_graph(self, path):
+        nodes, relationships = self.scan_directory(path)
+        self.graph_manager.create_nodes(nodes)
+        self.graph_manager.create_relationships(relationships)
+
+    def process_file(self, file_path, languaje):
+        path = Path(file_path)
+        if not path.exists():
+            print(f"File {file_path} does not exist.")
+            return
+        documents = SimpleDirectoryReader(
+            input_files=[path],
+            file_metadata=lambda x: {"filepath": x},
+        ).load_data()
+
+        code = CodeHierarchyNodeParser(
+            language=languaje,
+            chunk_min_characters=3,
+            code_splitter=CodeSplitter(
+                language=languaje, max_chars=1000, chunk_lines=10
+            ),
+        )
+
+        split_nodes = code.get_nodes_from_documents(documents)
+        node_list = []
+        edges_list = []
+        for node in split_nodes:
+            processed_node, relationships = self.__process_node__(node)
+            node_list.append(processed_node)
+            edges_list.extend(relationships)
+        return node_list, edges_list
+
+
+if __name__ == "__main__":
+    graph_manager = Neo4jManager()
+    graph_constructor = GraphConstructor(graph_manager)
+    graph_constructor.build_graph(".", "python")
+    graph_manager.close()
