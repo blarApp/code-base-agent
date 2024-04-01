0f82459bb95d55644da3e6fbacd8dbc2fd7d4391
berrazuriz1 <berrazuriz1@uc.cl>
2024-04-01 14:39:23 -0300

    feat: add unit test agent

---
+++ src/blar_graph/agents/agents/unit_test.py
@@ -0,0 +1,49 @@
+from langchain_core.utils.function_calling import convert_pydantic_to_openai_tool
+from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
+from langchain.agents.format_scratchpad.openai_tools import (
+    format_to_openai_tool_messages,
+)
+from langchain.agents.output_parsers.openai_tools import OpenAIToolsAgentOutputParser
+from blar_graph.agents.tools.KeywordSearchTool import KeywordSearchTool
+from blar_graph.db_managers.base_manager import BaseDBManager
+from langchain.agents import AgentExecutor
+from langchain_openai import ChatOpenAI
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+def get_unit_test_agent(graph_manager: BaseDBManager):
+    llm = ChatOpenAI(model="gpt-4-turbo-preview", temperature=0)
+
+    prompt = ChatPromptTemplate.from_messages(
+        [
+            (
+                "system",
+                """
+                You are a code assistant that makes solid and extensive unit test. You only respond with the unit test code and the test cases made in python.
+                You can traverse the graph by calling the function keword_search. 
+                You are given a graph of code functions, We purposly omited some code If the code has the comment '# Code replaced for brevity. See node_id ..... '. 
+                Prefer calling the function keword_search with query = node_id, only call it with starting nodes or neighbours. 
+                Extensivley traverse the graph before giving an answer
+                """,
+            ),
+            ("user", "{input}"),
+            MessagesPlaceholder(variable_name="agent_scratchpad"),
+        ]
+    )
+
+    tools = [KeywordSearchTool(db_manager=graph_manager)]
+    llm_with_tools = llm.bind_tools(tools)
+
+    agent = (
+        {
+            "input": lambda x: x["input"],
+            "agent_scratchpad": lambda x: format_to_openai_tool_messages(x["intermediate_steps"]),
+        }
+        | prompt
+        | llm_with_tools
+        | OpenAIToolsAgentOutputParser()
+    )
+
+    return AgentExecutor(agent=agent, tools=tools, verbose=True)
