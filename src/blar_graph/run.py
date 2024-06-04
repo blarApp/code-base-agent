d9dec1940c3d553a332a026b1b1528c62f79261e
berrazuriz1 <berrazuriz1@uc.cl>
2024-06-04 12:10:30 -0400

    change user_id for entityId

--- src/blar_graph/run.py
+++ src/blar_graph/run.py
@@ -3,8 +3,8 @@ import uuid
 from blar_graph.db_managers import Neo4jManager
 from blar_graph.graph_construction.core.graph_builder import GraphConstructor
 
-repo_id = str(uuid.uuid4())
-graph_manager = Neo4jManager(repo_id)
+repoId = str(uuid.uuid4())
+graph_manager = Neo4jManager(repoId, "company2")
 graph_constructor = GraphConstructor(graph_manager, "python")
 graph_constructor.build_graph("src")
 graph_manager.close()
