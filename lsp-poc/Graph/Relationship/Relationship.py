3833b04192e1f57d5d8114de5e905287bc2b86b2
v4rgas <66626747+v4rgas@users.noreply.github.com>
2024-11-04 17:24:11 -0300

    fix: add type hints and improve method signatures in Relationship and RelationshipCreator classes

--- lsp-poc/Graph/Relationship/Relationship.py
+++ lsp-poc/Graph/Relationship/Relationship.py
@@ -3,21 +3,25 @@ from Graph.Node import Node
 
 
 class Relationship:
+    start_node: Node
+    end_node: Node
+    rel_type: RelationshipType
+
     def __init__(self, start_node: Node, end_node: Node, rel_type: RelationshipType):
         self.start_node = start_node
         self.end_node = end_node
         self.rel_type = rel_type
 
     @property
-    def id(self):
+    def id(self) -> str:
         return self.__str__()
 
-    def as_object(self):
+    def as_object(self) -> dict:
         return {
             "sourceId": self.start_node.id,
             "targetId": self.end_node.id,
             "type": self.rel_type.name,
         }
 
-    def __str__(self):
+    def __str__(self) -> str:
         return f"{self.start_node} --[{self.rel_type}]-> {self.end_node}"
