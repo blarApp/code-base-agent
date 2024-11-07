f176ed2f245bdb1dfd9a17d2cf2bb6488eb40944
berrazuriz1 <berrazuriz1@uc.cl>
2024-11-07 09:30:24 -0300

    add relationship types by original node type

--- lsp-poc/TreeSitter/Languages/PythonDefinitions.py
+++ lsp-poc/TreeSitter/Languages/PythonDefinitions.py
@@ -6,6 +6,8 @@ from tree_sitter import Language
 
 from typing import Set
 
+from Graph.Node import NodeLabels
+
 
 class PythonDefinitions(LanguageDefinitions):
     def get_language() -> Language:
@@ -16,11 +18,19 @@ class PythonDefinitions(LanguageDefinitions):
 
     def get_relationships_group_types() -> dict[str, RelationshipType]:
         return {
-            "call": RelationshipType.CALLS,
-            "interpolation": RelationshipType.CALLS,
-            "import_from_statement": RelationshipType.IMPORTS,
-            "superclasses": RelationshipType.INHERITS,
-            "instantiation": RelationshipType.INSTANTIATES,
+            NodeLabels.CLASS: {
+                "import_from_statement": RelationshipType.IMPORTS,
+                "superclasses": RelationshipType.INHERITS,
+                "call": RelationshipType.INSTANTIATES,
+                "typing": RelationshipType.TYPING,
+                "assignment": RelationshipType.ASSIGNMENT,
+            },
+            NodeLabels.FUNCTION: {
+                "call": RelationshipType.CALLS,
+                "interpolation": RelationshipType.CALLS,
+                "import_from_statement": RelationshipType.IMPORTS,
+                "assignment": RelationshipType.ASSIGNMENT,
+            },
         }
 
     def get_function_call_query() -> str:
