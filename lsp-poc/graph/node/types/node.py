2595b5567d77d1cee02f78e21e71e54854d49976
berrazuriz1 <berrazuriz1@uc.cl>
2024-11-15 17:16:03 -0300

    add tatic method to replicate node id outside the package

--- lsp-poc/graph/node/types/node.py
+++ lsp-poc/graph/node/types/node.py
@@ -27,6 +27,10 @@ class Node:
     def is_path_format_valid(self) -> bool:
         return self.path.startswith("file://")
 
+    @staticmethod
+    def hash_path_to_id(path: str) -> str:
+        return md5(path.encode()).hexdigest()
+
     @property
     def id(self) -> str:
         return self.__str__()
