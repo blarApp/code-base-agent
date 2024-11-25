73b521d311913996feb65c4f5ef53eeac759007b
v4rgas <66626747+v4rgas@users.noreply.github.com>
2024-11-25 13:28:52 -0300

    feat: implement IdCalculator and add filtering methods to node classes

--- blarify/graph/node/file_node.py
+++ blarify/graph/node/file_node.py
@@ -6,6 +6,10 @@ class FileNode(DefinitionNode):
     def __init__(self, **kwargs):
         super().__init__(label=NodeLabels.FILE, **kwargs)
 
+    @property
+    def node_repr_for_identifier(self):
+        return "/" + self.name
+
     def as_object(self):
         obj = super().as_object()
         obj["attributes"]["text"] = self.code_text
--- blarify/main.py
+++ blarify/main.py
@@ -1,6 +1,6 @@
 from blarify.project_graph_creator import ProjectGraphCreator
 from blarify.project_file_explorer import ProjectFilesIterator
-from blarify.project_graph_diff_creator import ProjectGraphDiffCreator
+from blarify.project_graph_diff_creator import ProjectGraphDiffCreator, FileDiff, ChangeType
 from blarify.db_managers.neo4j_manager import Neo4jManager
 from blarify.code_references import LspQueryHelper
 
@@ -36,7 +36,7 @@ def main(root_uri: str = None, blarignore_path: str = None):
     lsp_query_helper.shutdown_exit_close()
 
 
-def main_diff(paths_to_create: list, root_uri: str = None, blarignore_path: str = None):
+def main_diff(file_diffs: list, root_uri: str = None, blarignore_path: str = None):
     lsp_query_helper = LspQueryHelper(root_uri=root_uri)
     lsp_query_helper.start()
 
@@ -53,7 +53,7 @@ def main_diff(paths_to_create: list, root_uri: str = None, blarignore_path: str 
         root_path=root_uri,
         lsp_query_helper=lsp_query_helper,
         project_files_iterator=project_files_iterator,
-        paths_to_create=paths_to_create,
+        file_diffs=file_diffs,
         diff_identifier="PR-123",
     )
 
@@ -61,13 +61,14 @@ def main_diff(paths_to_create: list, root_uri: str = None, blarignore_path: str 
 
     relationships = graph.get_relationships_as_objects()
     nodes = graph.get_nodes_as_objects()
-    nodes = [node for node in nodes if node["attributes"]["path"] in paths_to_create]
-    relationships = [
-        relationship
-        for relationship in relationships
-        if relationship["sourceId"] in [node["attributes"]["node_id"] for node in nodes]
-        or relationship["targetId"] in [node["attributes"]["node_id"] for node in nodes]
-    ]
+
+    # nodes = [node for node in nodes if node["attributes"]["path"] in [file_diff.path for file_diff in file_diffs]]
+    # relationships = [
+    #     relationship
+    #     for relationship in relationships
+    #     if relationship["sourceId"] in [node["attributes"]["node_id"] for node in nodes]
+    #     or relationship["targetId"] in [node["attributes"]["node_id"] for node in nodes]
+    # ]
 
     print(f"Saving graph with {len(nodes)} nodes and {len(relationships)} relationships")
     graph_manager.save_graph(nodes, relationships)
@@ -81,7 +82,23 @@ if __name__ == "__main__":
     blarignore_path = os.getenv("BLARIGNORE_PATH")
     main(root_uri=root_path, blarignore_path=blarignore_path)
     main_diff(
-        paths_to_create=["file:///home/juan/devel/blar/lsp-poc/blarify/graph/node/utils/node_factory.py"],
+        file_diffs=[
+            FileDiff(
+                path="file:///home/juan/devel/blar/lsp-poc/blarify/graph/node/utils/node_factory.py",
+                diff_text="diff+++",
+                change_type=ChangeType.MODIFIED,
+            ),
+            FileDiff(
+                path="file:///home/juan/devel/blar/lsp-poc/blarify/graph/relationship/relationship_type.py",
+                diff_text="diff+++",
+                change_type=ChangeType.MODIFIED,
+            ),
+            FileDiff(
+                path="file:///home/juan/devel/blar/lsp-poc/blarify/graph/relationship/relationship_creator.py",
+                diff_text="diff+++",
+                change_type=ChangeType.ADDED,
+            ),
+        ],
         root_uri=root_path,
         blarignore_path=blarignore_path,
     )
