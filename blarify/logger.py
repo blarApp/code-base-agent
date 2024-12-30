282d328fb27e7a521fcf35ad97adb4ba142d7a57
v4rgas <66626747+v4rgas@users.noreply.github.com>
2024-12-30 13:44:27 -0300

    refactor: modify Logger to always print messages, removing DEBUG check

--- blarify/logger.py
+++ blarify/logger.py
@@ -7,5 +7,5 @@ logger = logging.getLogger(__name__)
 class Logger:
     @staticmethod
     def log(message: str) -> None:
-        if os.getenv("DEBUG"):
-            print(message)
+        # if os.getenv("DEBUG"):
+        print(message)
