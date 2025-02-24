2f522f6ec36cb1ba6854b5672b730d858e1a6f7c
Juan Vargas <66626747+v4rgas@users.noreply.github.com>
2025-02-24 17:45:27 -0300

    Update graph_builder.py

--- blarify/examples/graph_builder.py
+++ blarify/examples/graph_builder.py
@@ -7,9 +7,7 @@ import os
 
 
 def build(root_path: str = None):
-    graph_builder = GraphBuilder(
-        root_path=root_path, extensions_to_skip=[".json"], names_to_skip=["__pycache__"]
-    )
+    graph_builder = GraphBuilder(root_path=root_path, extensions_to_skip=[".json"], names_to_skip=["__pycache__"])
     graph = graph_builder.build()
 
     relationships = graph.get_relationships_as_objects()
