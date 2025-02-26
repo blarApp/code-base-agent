a3d5bc3ffe2b174be3b54d04825f16ce7901d4f5
v4rgas <66626747+v4rgas@users.noreply.github.com>
2025-02-26 14:53:35 -0300

    chore: update graph builder to skip additional directories

--- blarify/examples/graph_builder.py
+++ blarify/examples/graph_builder.py
@@ -7,7 +7,9 @@ import os
 
 
 def build(root_path: str = None):
-    graph_builder = GraphBuilder(root_path=root_path, extensions_to_skip=[".json"], names_to_skip=["__pycache__"])
+    graph_builder = GraphBuilder(
+        root_path=root_path, extensions_to_skip=[".json"], names_to_skip=["__pycache__", ".venv", ".git"]
+    )
     graph = graph_builder.build()
 
     relationships = graph.get_relationships_as_objects()
--- blarify/vendor/multilspy/multilspy_config.py
+++ blarify/vendor/multilspy/multilspy_config.py
@@ -5,6 +5,7 @@ Configuration parameters for Multilspy.
 from enum import Enum
 from dataclasses import dataclass
 
+
 class Language(str, Enum):
     """
     Possible languages with Multilspy.
@@ -18,15 +19,18 @@ class Language(str, Enum):
     JAVASCRIPT = "javascript"
     GO = "go"
     RUBY = "ruby"
+    DART = "dart"
 
     def __str__(self) -> str:
         return self.value
+
 
 @dataclass
 class MultilspyConfig:
     """
     Configuration parameters
     """
+
     code_language: Language
     trace_lsp_communication: bool = False
 
@@ -36,7 +40,7 @@ class MultilspyConfig:
         Create a MultilspyConfig instance from a dictionary
         """
         import inspect
-        return cls(**{
-            k: v for k, v in env.items() 
-            if k in inspect.signature(cls).parameters
-        })
+
+        return cls(
+            **{k: v for k, v in env.items() if k in inspect.signature(cls).parameters}
+        )
